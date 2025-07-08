1d08420b4bee0e6b2667e95b74a55f519558455f
Aditya-raj11 <adityaraj110405@gmail.com>
2025-07-08 23:00:43 +0530

    toastification setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,121 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-// import 'router.dart';
-// import 'providers.dart';
+import 'router.dart';
+import 'providers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp.router(
+        title: 'Nourish App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        routerConfig: router,
+      ),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:toastification/toastification.dart';
+// import 'package:nourish_app/widgets/utils/toast_service.dart';
 
 // void main() {
-//   runApp(const MyApp());
+//   runApp(MyApp());
 // }
 
 // class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
+//   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+//   MyApp({super.key});
 //   @override
 //   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: providers,
-//       child: MaterialApp.router(
-//         title: 'Nourish App',
-//         theme: ThemeData(
-//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         ),
-//         routerConfig: router,
+//     return ToastificationWrapper(
+//       config: ToastificationConfig(
+//         alignment: Alignment.bottomCenter,
+//         maxToastLimit: 2,
+//       ),
+//       child: MaterialApp(
+//         navigatorKey: navigatorKey,
+//         home: HomeScreen(),
+
+//         // ✅ This gives you a proper context *after* Directionality is available
+//         builder: (context, child) {
+//           WidgetsBinding.instance.addPostFrameCallback((_) {
+//             ToastService();
+//           });
+//           return child!;
+//         },
 //       ),
 //     );
 //   }
 // }
 
-import 'package:flutter/material.dart';
-import 'package:toastification/toastification.dart';
-import 'package:nourish_app/widgets/utils/toast_service.dart';
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-  MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return ToastificationWrapper(
-      config: ToastificationConfig(
-        alignment: Alignment.bottomCenter,
-        maxToastLimit: 2,
-      ),
-      child: MaterialApp(
-        navigatorKey: navigatorKey,
-        home: HomeScreen(),
-
-        // ✅ This gives you a proper context *after* Directionality is available
-        builder: (context, child) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            ToastService();
-          });
-          return child!;
-        },
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                ToastService().showToast(
-                  context,
-                  'This is toast service',
-                  type: ToastificationType.success,
-                );
-              },
-              child: Text('Show Toast'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ToastService().showToast(
-                  context,
-                  'This is toast service',
-                  type: ToastificationType.error,
-                );
-              },
-              child: Text('Show Toast'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ToastService().showToast(
-                  context,
-                  'This is toast service',
-                  type: ToastificationType.info,
-                );
-              },
-              child: Text('Show Toast'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                ToastService().showToast(
-                  context,
-                  'This is toast service',
-                  type: ToastificationType.warning,
-                  description: 'hiiiiiiiiiiiiiiiiiiii how are you',
-                );
-              },
-              child: Text('Show Toast'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Home')),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             ElevatedButton(
+//               onPressed: () {
+//                 ToastService().showToast(
+//                   context,
+//                   'This is toast service',
+//                   type: ToastificationType.success,
+//                 );
+//               },
+//               child: Text('Show Toast'),
+//             ),
+//             ElevatedButton(
+//               onPressed: () {
+//                 ToastService().showToast(
+//                   context,
+//                   'This is toast service',
+//                   type: ToastificationType.error,
+//                 );
+//               },
+//               child: Text('Show Toast'),
+//             ),
+//             ElevatedButton(
+//               onPressed: () {
+//                 ToastService().showToast(
+//                   context,
+//                   'This is toast service',
+//                   type: ToastificationType.info,
+//                 );
+//               },
+//               child: Text('Show Toast'),
+//             ),
+//             ElevatedButton(
+//               onPressed: () {
+//                 ToastService().showToast(
+//                   context,
+//                   'This is toast service',
+//                   type: ToastificationType.warning,
+//                   description: 'hiiiiiiiiiiiiiiiiiiii how are you',
+//                 );
+//               },
+//               child: Text('Show Toast'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
