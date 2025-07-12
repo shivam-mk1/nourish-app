116771a55f8f439ab4a7de5fb311cf64d2cce2d4
Aditya-raj11 <adityaraj110405@gmail.com>
2025-07-12 12:01:43 +0530

    toastification setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
 
-// import 'router.dart';
-// import 'providers.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: providers,
-//       child: MaterialApp.router(
-//         title: 'Nourish App',
-//         theme: ThemeData(
-//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         ),
-//         routerConfig: router,
-//       ),
-//     );
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
