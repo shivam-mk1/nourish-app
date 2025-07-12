34dab0f71f32208ccedd5c196c1112dccd1b09f6
Aditya-raj11 <adityaraj110405@gmail.com>
2025-07-12 12:00:52 +0530

    toastification setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,38 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// import 'router.dart';
+// import 'providers.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: providers,
+//       child: MaterialApp.router(
+//         title: 'Nourish App',
+//         theme: ThemeData(
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         ),
+//         routerConfig: router,
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:toastification/toastification.dart';
 
 import 'router.dart';
 import 'providers.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,14 +43,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: providers,
-      child: MaterialApp.router(
-        title: 'Nourish App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ToastificationWrapper(
+      config: ToastificationConfig(
+        alignment: Alignment.bottomCenter,
+        maxToastLimit: 2,
+      ),
+      child: MultiProvider(
+        providers: providers,
+        child: MaterialApp.router(
+          title: 'Nourish App',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          routerConfig: router,
+          builder: (context, child) => child!,
         ),
-        routerConfig: router,
       ),
     );
   }
